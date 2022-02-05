82868752317c6695f7168b36bf717631452f51cc
Alexander <alez.grante@gmail.com>
2022-02-05 20:15:37 +0300

    tests added

---
+++ app/testlearningmodule/src/test/java/com/example/testlearningmodule/TriangleTest.kt
@@ -0,0 +1,23 @@
+package com.example.testlearningmodule
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class TriangleTest{
+
+    @Test
+    fun test_valid_data(){
+        val triangle = Triangle(3,4,5)
+        val actual = triangle.isRightTriangle()
+        val expected = true
+        assertEquals(expected,actual)
+    }
+
+    @Test
+    fun test_negative_data(){
+        val triangle = Triangle(-3,-4,-5)
+        val actual = triangle.isRightTriangle()
+        val expected = false
+        assertEquals(expected,actual)
+    }
+}
